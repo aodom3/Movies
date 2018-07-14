ae7377f59e9aa6a60125161504f027ef79b4a06f
aodom3 <aodom3@gmail.com>
2018-07-14 14:59:25 -0400

    added user

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,19 +4,19 @@ require 'pp'
 class MoviesController < ApplicationController
 
 def index
-    # @movies = movie.generate
-    # @movies = current_user.movies.map do |movie|
-        # url = "http://www.omdbapi.com/?i=#{movie.omdb_id}&apikey=f3d1e28d"
-        # response = HTTParty.get(url)
-        # response_body = JSON.parse(response.body)
+    @movies = movie.generate
+    @movies = current_user.movies.map do |movie|
+        url = "http://www.omdbapi.com/?i=#{movie.omdb_id}&apikey=f3d1e28d"
+        response = HTTParty.get(url)
+        response_body = JSON.parse(response.body)
   
-        # movie.title = response_body['Title']
-        # movie.genre = response_body ['Genre']
-        # movie.release_year = response_body['Year']
-        # movie.synopsis = response_body['Synopsis']
-        # movie.picture = response_body ['Picture']
-        # movie.favorite = release_year['favorite']
+        movie.title = response_body['Title']
+        movie.genre = response_body ['Genre']
+        movie.release_year = response_body['Year']
+        movie.synopsis = response_body['Synopsis']
+        movie.picture = response_body ['Picture']
+        movie.favorite = release_year['favorite']
       end
     end
 
-# end
+end
